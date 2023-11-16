213d366b377c92d97128811aa4c03a2791ec5389
JGatlinIIT <143561844+JGatlinIIT@users.noreply.github.com>
2023-11-16 10:33:11 -0600

    added my schools location to script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.690948, lng: -87.718256 },
+    zoom: 15,
+  });
 
+  var marker = new google.maps.Marker({
+    position: { lat: 41.690948, lng: -87.718256 },
+    map: map,
+  });
